d8a709a844e3bda503698a9e80cc2aa843287f16
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-09-13 10:41:10 +0200

    Add files via upload

---
+++ Programacion/Python/Ejemplos/Ejemplo_1.py
@@ -0,0 +1,14 @@
+#
+
+var_entero=4
+var_dec=3.5
+var_text="hola"
+
+#
+
+print("el resutado es",var_entero,"pomas")
+print(var_dec)
+print(var_text)
+
+var_total=var_entero+var_dec
+print(f"el resultado de",(var_entero), "y", (var_dec), "es",var_total)
