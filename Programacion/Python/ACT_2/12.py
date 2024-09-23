fd0941a475ab54fc915bcdaaa95bade5369cb7eb
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-09-23 15:18:02 +0200

    Add files via upload

---
+++ Programacion/Python/ACT_2/12.py
@@ -0,0 +1,8 @@
+var1=int(input("introduce el valor de lado de un trapecio isosceles:"))
+var2=int(input("introduce el base menor:"))
+var3=int(input("introduce el base mayor:"))
+var4=int(input("introduce la altura:"))
+var_p=var2+var3+2*var1
+var_a=(var2+var3)/2*var4
+print("el perimetro es:",(var_p))
+print("el area es:",(var_a))
