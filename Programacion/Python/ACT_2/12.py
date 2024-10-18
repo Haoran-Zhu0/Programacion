e2d2c03a54cbfaad6d86a70ae701d2339a83cab2
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-10-18 10:29:59 +0200

    12.py

--- Programacion/Python/ACT_2/12.py
+++ Programacion/Python/ACT_2/12.py
@@ -1,3 +1,6 @@
+# Realiza un programa que, introduciendo en los valores de lado, base menor, base mayor 
+#y altura de un trapecio isósceles, nos devuelva por pantalla en el área y el perímetro
+
 var1=int(input("introduce el valor de lado de un trapecio isosceles:"))
 var2=int(input("introduce el base menor:"))
 var3=int(input("introduce el base mayor:"))
