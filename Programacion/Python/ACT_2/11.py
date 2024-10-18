050cb4d60131107d77bd9531b7b4f3ba6de981bb
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-10-18 10:29:35 +0200

    11.py

--- Programacion/Python/ACT_2/11.py
+++ Programacion/Python/ACT_2/11.py
@@ -1,3 +1,6 @@
+#Realiza un programa que introduciendo el valor del lado de un cuadrado nos devuelva 
+#por pantalla en el área y el perímetro
+
 var1=int(input("introduce el valor del lado de un cuadrodo;"))
 var_p=var1*4
 var_a=var1*var1
