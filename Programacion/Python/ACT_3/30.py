e56d2c0f57a86d503dea1d9b558b7346947d1896
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-10-25 10:02:24 +0200

    Add files via upload

---
+++ Programacion/Python/ACT_3/30.py
@@ -0,0 +1,8 @@
+var1=input("Intrpduce una frase:")
+len=len(var1)
+if len==11:
+    print("La frase tiene 11 caracteres.")
+elif len<11:
+    print("La frase tiene menos de 11 caracteres.")
+elif len>11:
+    print("La frase tiene más de 11 caracteres.")
---
+++ Programacion/Python/ACT_3/31.py
@@ -0,0 +1,15 @@
+var1="A qui幯 madruga Dios ayuda"
+print(var1)
+var2=input("Introduce la palabra:")
+if var2 not in var1:
+    print("La palabra no esta en la frase")
+elif var2=="A":
+    print("La palabra esta en la frase y esta en el i要dice 1")
+elif var2=="qui幯":
+    print("La palabra esta en la frase y esta en el i要dice 2")
+elif var2=="madruga":
+    print("La palabra esta en la frase y esta en el i要dice 8")
+elif var2=="Dios":
+    print("La palabra esta en la frase y esta en el i要dice 16")
+elif var2=="ayuda":
+    print("La palabra esta en la frase y esta en el indice 21")
