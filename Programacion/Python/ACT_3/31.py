1072a1371016313ef32676a07c0536e066fd68e0
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-10-28 10:29:22 +0100

    31.py

--- Programacion/Python/ACT_3/31.py
+++ Programacion/Python/ACT_3/31.py
@@ -1,15 +1,19 @@
-var1="A quién madruga Dios ayuda"
+#Asigna a una variable de texto la siguiente frase: A quiÃ©n madruga Dios ayuda. 
+#Comprueba si existen las siguientes palabras mostrando por pantalla la posiciÃ³n de su 
+#Ã­ndice.
+
+var1="A quiÃ©n madruga Dios ayuda"
 print(var1)
 var2=input("Introduce la palabra:")
 if var2 not in var1:
     print("La palabra no esta en la frase")
 elif var2=="A":
-    print("La palabra esta en la frase y esta en el i­ndice 1")
-elif var2=="quién":
-    print("La palabra esta en la frase y esta en el i­ndice 2")
+    print("La palabra esta en la frase y esta en el iÂ­ndice 1")
+elif var2=="quiÃ©n":
+    print("La palabra esta en la frase y esta en el iÂ­ndice 2")
 elif var2=="madruga":
-    print("La palabra esta en la frase y esta en el i­ndice 8")
+    print("La palabra esta en la frase y esta en el iÂ­ndice 8")
 elif var2=="Dios":
-    print("La palabra esta en la frase y esta en el i­ndice 16")
+    print("La palabra esta en la frase y esta en el iÂ­ndice 16")
 elif var2=="ayuda":
     print("La palabra esta en la frase y esta en el indice 21")
