46f1c7f25f69e6deaee30edd876876317b7ed72e
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-10-28 10:30:57 +0100

    29.py

--- Programacion/Python/ACT_3/29.py
+++ Programacion/Python/ACT_3/29.py
@@ -1,5 +1,5 @@
 #Busca Internet qué función permite obtener la longitud de un String, realiza un programa 
-#que al introducir una frase devuelva la longitud
+#que al introducir una frase devuelva la longitud.
 
 var1=input("Intrpduce una frase:")
 l=var1
