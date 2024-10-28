d52cabf221f3dc151a79883dd1c5ed5e8742894b
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-10-28 10:24:45 +0100

    Add files via upload

---
+++ Programacion/Python/ACT_3/32.py
@@ -0,0 +1,16 @@
+var1="A qui幯 madruga Dios ayuda"
+print(var1)
+var3=var1.islower()
+var2=input("Introduce la palabra:")
+var4=var1.islower()
+
+if var2=="a":
+    print("La palabra esta en la frase y esta en el i要dice 1")
+elif var2=="qui幯":
+    print("La palabra esta en la frase y esta en el i要dice 2")
+elif var2=="madruga":
+    print("La palabra esta en la frase y esta en el i要dice 8")
+elif var2=="dios":
+    print("La palabra esta en la frase y esta en el i要dice 16")
+elif var2=="ayuda":
+    print("La palabra esta en la frase y esta en el idice 21")
---
+++ Programacion/Python/ACT_3/33.py
@@ -0,0 +1,7 @@
+var1=input("Introduce una frase:")
+var_a=var1.count("a")
+var_e=var1.count("e")
+var_i=var1.count("i")
+var_o=var1.count("o")
+var_u=var1.count("u")
+print("El número de a es",(var_a)," el número de e es",(var_e)," el número de i es",(var_i)," el número de o es ",(var_o)," y el número de u es",(var_u))
