dff5f1b6a2fdf962cb0eae080294b5ebdf44ae79
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-10-28 10:32:53 +0100

    21.py

--- Programacion/Python/ACT_3/21.py
+++ Programacion/Python/ACT_3/21.py
@@ -1,5 +1,5 @@
 #Programa que calcula una ecuación de segundo grado. Controla que el valor de la raíz 
-#cuadrada no de un valor negativo
+#cuadrada no de un valor negativo.
 
 a=int(input("Introduce el primer valor:"))
 b=int(input("Introduce el segundo valor:"))
