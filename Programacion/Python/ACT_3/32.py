3b52ac6ffe38f34203fb407aff5863750cc3c959
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-10-28 10:29:47 +0100

    32.py

--- Programacion/Python/ACT_3/32.py
+++ Programacion/Python/ACT_3/32.py
@@ -1,16 +1,19 @@
-var1="A quién madruga Dios ayuda"
+#CÃ³mo solucionarÃ­as con ayuda de mÃ©todos String el problema del ejercicio anterior para 
+#no distinguir entre mayÃºsculas y minÃºsculas.
+
+var1="A quiÃ©n madruga Dios ayuda"
 print(var1)
 var3=var1.islower()
 var2=input("Introduce la palabra:")
 var4=var1.islower()
 
 if var2=="a":
-    print("La palabra esta en la frase y esta en el i­ndice 1")
-elif var2=="quién":
-    print("La palabra esta en la frase y esta en el i­ndice 2")
+    print("La palabra esta en la frase y esta en el iÂ­ndice 1")
+elif var2=="quiÃ©n":
+    print("La palabra esta en la frase y esta en el iÂ­ndice 2")
 elif var2=="madruga":
-    print("La palabra esta en la frase y esta en el i­ndice 8")
+    print("La palabra esta en la frase y esta en el iÂ­ndice 8")
 elif var2=="dios":
-    print("La palabra esta en la frase y esta en el i­ndice 16")
+    print("La palabra esta en la frase y esta en el iÂ­ndice 16")
 elif var2=="ayuda":
     print("La palabra esta en la frase y esta en el idice 21")
