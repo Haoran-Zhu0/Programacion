9b1986868d02472967a22017fd18ad75a5e98f1d
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-10-28 10:33:09 +0100

    26.py

--- Programacion/Python/ACT_3/26.py
+++ Programacion/Python/ACT_3/26.py
@@ -1,5 +1,5 @@
 #Realiza un programa que, al introducir una letra por teclado, aparezca por pantalla si 
-#está o no en mayúscula. Utiliza dos IF’s que establezcan True o False a la condición
+#está o no en mayúscula. Utiliza dos IF’s que establezcan True o False a la condición.
 
 var1=input("introduce una letra:")
 l=var1.isupper()
