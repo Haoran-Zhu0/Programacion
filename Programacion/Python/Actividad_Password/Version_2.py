89bdf10e64cbb4e542ef9f454599ffa2b3e130d4
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2024-12-13 10:37:39 +0100

    Version_2.py

--- Programacion/Python/Actividad_Password/Version_2.py
+++ Programacion/Python/Actividad_Password/Version_2.py
@@ -1,42 +1,46 @@
 print("La longitud del password debe ser entre 6 y 8 caracteres")
-print("Debe contener, al menos 3 números del 1 al 9, 2 letras mayúsculas o minúsculas y 2 símbolos(*_@/&#)")
-print("Un numero mayor o igual 1 y menor o igual 5")
+print("Debe contener 3 números, 2 letras y 2 símbolos")
+print("Debe tener 2 minusculas y 1 mayuscula")
+print("Dos numero mayor o igual 1 y menor o igual 5")
 print("Un numero mayor o igual 6 y menor o igual 9")
+print("Los simbolos deben de ser los siguiente:*_@/&#")
 print("Debes introducir 3 contraseñas")
 correctas=0
 incorrectas=0
+numeros=0
+minusculas=0
+mayusculas=0
+simbolos=0
+es_correcta=True
 for contador in range(3):
     var1=input(f"Introduce la contraseña {contador+1}:")    
     if len(var1)<6 or len(var1)>8:
-        print("Error: La contraseña debe tener entre 6 y 8 caracteres")
-        incorrectas+=1
-else:
-    numeros=0
-    letras_minusculas=0
-    letras_mayusculas=0
-    simbolos=0
-    for x in var1:
-        if not x.isnumeric:
-            print("Debe tener 3 numeros del 1 al 9")
-        elif x.isnumeric():
+        print("Error: La contraseña debe tener entre 6 y 8 caracteres.")
+        es_correcta=False
+for x in var1:
+    if x.isnumeric():
+        if'1'<=x<='5':
             numeros+=1
-        if not x.isalpha():
-            print("Debe tener dos letras(mayúsculas o minúsculas)")
-        elif x.islower():
-            letras_minusculas+=1
-        elif x.isupper():
-            letras_mayusculas+=1
-        if not x in "*@/&#":
-            print("Debe tener 2 simbolos(*@/&#)")
-        elif x in "*_@/&#":
-            simbolos+=1
-        
-    if numeros>=3 and letras_minusculas>=2 and letras_mayusculas>=1 and simbolos>=2:
-        print("La contraseña es correcta")
-        correctas+=1
+    elif'6'<=x<='9':
+        numeros+=1
+    elif x.isalpha():
+        if x.islower():
+            minusculas+=1
+    elif x.isupper():
+        mayusculas+=1
+    elif x in "*_@/&":
+        simbolos+=1
     else:
-        print("La contraseña no cumple con los requisitos")
-        incorrectas+=1
-
-print(f"Contraseñas correctas:{correctas}")
-print(f"Contraseñas incorrectas:{incorrectas}")
+        print("Error: La contraseña contiene caracteres no válidos.")
+        es_correcta=False
+        if es_correcta:
+            if (numeros==3 and minusculas>=2 and mayusculas>=1 and simbolos>=2):
+                print("La contraseña es correcta.")
+                correctas+=1
+else:
+    print("La contraseña no cumple con los requisitos.")
+    es_correcta=False
+if not es_correcta:
+    incorrectas+=1
+print(f"Contraseñas correctas: {correctas}")
+print(f"Contraseñas incorrectas: {incorrectas}")
