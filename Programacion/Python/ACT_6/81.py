5a9cb801aaec330d3461059214543e7c6ca46a48
Haoran-Zhu0 <haoran.zhu@campus.xaloc.org>
2025-03-27 09:37:51 +0100

    81.py

--- Programacion/Python/ACT_6/81.py
+++ Programacion/Python/ACT_6/81.py
@@ -1,6 +1,6 @@
 #A partir de una lista definida, busca el método apropiado para que se visualice un valor de la 
 #lista al azar.
 
-import random:
-    
-lista=[casa,barco,gato,perro,madera,agua,puente,pantalón]
+import random
+lista=["casa", "barco", "gato", "perro", "madera", "agua", "puente", "pantalón"]
+print(lista[random.randint(0, len(lista)-1)])
